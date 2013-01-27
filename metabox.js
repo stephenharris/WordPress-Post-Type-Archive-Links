59d0b45b7ed3aaf7f4d294fae884b693631eefd9
Ryan Urban <ryan@fringewebdevelopment.com>
2013-01-27 17:00:55 -0500

    Update ajax nonce and action handlers
    In following through with the updates to main plugin file for ajax nonce and action bits, updating our js

--- metabox.js
+++ metabox.js
@@ -1,22 +1,28 @@
-     jQuery(document).ready(function($) {
-          $('#submit-post-type-archives').click(function(event){
-               event.preventDefault();
+/**
+ * Handle the custom post type nav menu meta box
+ */
+jQuery( document ).ready( function($) {
+     $( '#submit-post-type-archives' ).click( function( event ) {
+		event.preventDefault();
 
-               /* Get checked boxes */
-               var postTypes = [];
-               $('#post-type-archive-checklist li :checked').each(function() {
-                    postTypes.push($(this).val());
-               });
+		/* Get checked boxes */
+		var postTypes = [];
+		$( '#' + hptal_obj.metabox_id + ' li :checked' ).each( function() {
+			postTypes.push( $( this ).val() );
+		} );
 
-               /* Send checked post types with our action, and nonce */
-               $.post( ajaxurl, {
-                         action: "my-add-post-type-archive-links",
-                         posttypearchive_nonce: MyPostTypeArchiveLinks.nonce,
-                         post_types: postTypes,
-                    },
+		/* Send checked post types with our action, and nonce */
+		$.post( hptal_obj.ajaxurl, {
+				action: hptal_obj.action,
+				posttypearchive_nonce: hptal_obj.nonce,
+				post_types: postTypes,
+				nonce: hptal_obj.nonce
+			},
 
-                    /* AJAX returns html to add to the menu */
-                    function( response ) {$('#menu-to-edit').append(response);}
-               );
-          })
-     });
+			/* AJAX returns html to add to the menu */
+			function( response ) {
+				$( '#menu-to-edit' ).append( response );
+			}
+		);
+	} );
+} );
